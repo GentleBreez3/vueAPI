bc378f1b346d317daf814cdd0733e902ea72ae91
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-07-19 16:59:46 +0900

    vue api homework

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <VueAPI />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import VueAPI from './components/VueAPI.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    VueAPI
   }
 }
 </script>
---
+++ src/components/VueAPI.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="photo in photos" v-bind:key="photo.id">
+      <h3>Photo title: {{ photo.title }}</h3>
+      <p>Photo url: {{ photo.url }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      photos: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.axios.get(
+          "https://jsonplaceholder.typicode.com/photos"
+        );
+		console.log(response.data);
+        this.photos = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
